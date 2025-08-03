89564d73bc4a9a3442bfe89c2efba4feb3219d42
RaymondBrien <raytbrien@gmail.com>
2025-08-03 18:52:54 +0100

    Add configurable request types as enum classes

--- prompt_templates/prompt_config.py
+++ prompt_templates/prompt_config.py
@@ -1,26 +1,10 @@
 from typing import Dict
 from pathlib import Path
+from enum import Enum
 
 import yaml
 
 from API_CONFIG import MAX_TOKENS
-
-
-def validate_token_length(text: str, MAX_TOKENS) -> str:
-    if len(text) < MAX_TOKENS:
-        return text
-    else: 
-        raise RuntimeError()  # TODO
-
-def get_topic_dict(filename: str):
-    with open(f"{filename}.yml", 'r',) as f:
-        output = yaml.safe_load(f)
-    print(output)
-    return output
-
-def match_topic_with_file() -> Path:
-    ...
-
 
 
 class Role:
@@ -28,15 +12,55 @@ class Role:
         self.default = """knowledgeable technical assistant and expert in setting up
         developers with the most relevant and future-proof skills"""
 
-        self.custom: str = ""
+        self.role: str = self.default
 
     def custom_role(self) -> str:
-        self.role = input("Type a custom role here: ")
+        self.custom = input("Type a custom role here: ")
         return self.role
 
 
 class Topic:
     def __init__(self):
-        self.topic: Str = ""
-        self.topic_file: Dict = get_topic_dict(self.topic)  # TODO fill out func above to find topic file by matching text with file name from topics dir
+        self.topic: str ""
+        self.topic_file: Dict = self.get_topic_dict(self.topic)  # TODO fill out func above to find topic file by matching text with file name from topics dir
         
+
+    def get_topic_dict(self, filename: str):
+        with open(f"{filename}.yml", 'r',) as f:
+            output = yaml.safe_load(f)
+        print(output)
+        return output
+
+
+class PromptType(Enum):
+    """TODO add more, according to anthropic docs"""
+    TEXT = "text"
+    DOCUMENT = "document"
+
+
+class MessageRole(Enum):
+    USER = "user"
+    ASSISTANT = "assistant"
+
+
+class RequestReturnType(Enum):
+    MARKDOWN = "markdown"
+    TEXT = "text"
+    YAML = "yaml"
+    CODE = "code"
+    FILE = "file"
+
+class TargettedOs(Enum):
+    LINUX = "linux"
+    WINDOWS = "windows"
+    MACOS = "macos"
+
+
+def validate_token_length(text: str, MAX_TOKENS) -> str:
+    if len(text) < MAX_TOKENS:
+        return text
+    else:
+        raise RuntimeError()  # TODO
+
+def match_topic_with_file() -> Path:
+    return
